2f990585a85c2d81a9f1bfb1afae49c51870153c
Andreas691667 <andreaskaag@hotmail.com>
2023-05-05 09:51:03 +0200

    inserted comments and formatted

--- PGLEventManagerController.py
+++ PGLEventManagerController.py
@@ -46,8 +46,6 @@ class PGLEventManagerController:
         self.__mqtt_client.on_message = self.__onMessage
         self.__mqtt_client.on_connect = self.__onConnect
         self.__mqtt_client.on_disconnect = self.__onDisconnect
-        # self.__mqtt_client.on_disconnect = self.__onDisconnect
-        # initialize other callback methods here
 
     # connect the model to address and start the subscriber_thread
     def startListening(self) -> None:
@@ -69,10 +67,12 @@ class PGLEventManagerController:
         while not self.__events_queue.empty():
             pass
 
+        # stop subscriber thread
         self.__stop___worker.set()
-        self.__mqtt_client.loop_stop()
+        self.__mqtt_client.loop_stop()                          # stop mqtt loop
+        # unsubscribe from all topics
         self.__mqtt_client.unsubscribe(self.__REQUEST_TOPICS)
-        self.__mqtt_client.disconnect()
+        self.__mqtt_client.disconnect()                         # disconnect from mqtt
 
         # Disconnect from the database
         self.__PGLmodel.disconnectDB()
@@ -84,14 +84,22 @@ class PGLEventManagerController:
     # callback method that is called when __mqtt_client is disconnected
     def __onDisconnect(self, client, userdata, rc, _) -> None:
         # publish an empty message to all request topics with retain=True to clear the mqtt queue
-        self.__mqtt_client.publish(self.__REQUEST_STORE_EVENT_IN_DB_TOPIC, "", retain=True)
-        self.__mqtt_client.publish(self.__REQUEST_EMERGENCY_TOPIC, "", retain=True)
-        self.__mqtt_client.publish(self.__REQUEST_STORE_USER_IN_DB_TOPIC, "", retain=True)
-        self.__mqtt_client.publish(self.__REQUEST_CREATE_PRODUCT_TOPIC, "", retain=True)
-        self.__mqtt_client.publish(self.__REQUEST_GET_EVENTS_TOPIC, "", retain=True)
-        self.__mqtt_client.publish(self.__REQUEST_GET_EMERGENCIES_TOPIC, "", retain=True)
-        self.__mqtt_client.publish(self.__REQUEST_VALIDATE_USER_TOPIC, "", retain=True)
-        self.__mqtt_client.publish(self.__REQUEST_NEW_DEVICE_TOPIC, "", retain=True)
+        self.__mqtt_client.publish(
+            self.__REQUEST_STORE_EVENT_IN_DB_TOPIC, "", retain=True)
+        self.__mqtt_client.publish(
+            self.__REQUEST_EMERGENCY_TOPIC, "", retain=True)
+        self.__mqtt_client.publish(
+            self.__REQUEST_STORE_USER_IN_DB_TOPIC, "", retain=True)
+        self.__mqtt_client.publish(
+            self.__REQUEST_CREATE_PRODUCT_TOPIC, "", retain=True)
+        self.__mqtt_client.publish(
+            self.__REQUEST_GET_EVENTS_TOPIC, "", retain=True)
+        self.__mqtt_client.publish(
+            self.__REQUEST_GET_EMERGENCIES_TOPIC, "", retain=True)
+        self.__mqtt_client.publish(
+            self.__REQUEST_VALIDATE_USER_TOPIC, "", retain=True)
+        self.__mqtt_client.publish(
+            self.__REQUEST_NEW_DEVICE_TOPIC, "", retain=True)
         print("MQTT client disconnected")
 
     # callback method that is called whenever a message arrives on a topic that '__mqtt_client' subscribes to
@@ -99,6 +107,7 @@ class PGLEventManagerController:
         if message.payload == b'':
             print("Empty MQTT message received")
         else:
+            # put message in queue
             self.__events_queue.put(message)
             print(f'MQTT Message received with payload: {message.payload}')
 
@@ -109,14 +118,14 @@ class PGLEventManagerController:
         print("Subscriber_thread __worker started")
         while not self.__stop___worker.is_set():
             try:
-                # pull message form events queue
+                # pull message from events queue
                 # timeout indicates that we stop trying to dequeue after 1 s
                 # throws 'Empty' exception if timeout
                 mqtt_message: MQTTMessage = self.__events_queue.get(timeout=1)
             # if queue empty return
             except Empty:
                 pass
-            # if the pull was succesful, handle the message
+            # if the pull was succesful, handle the message to corresponding topic
             else:
                 try:
                     mqtt_message_topic = mqtt_message.topic
@@ -125,56 +134,63 @@ class PGLEventManagerController:
                             event_string = mqtt_message.payload.decode("utf-8")
                             self.__PGLmodel.storeDevice(event_string)
 
-                        # store event from PI in database
+                        # store journey from PI in database
                         case self.__REQUEST_STORE_EVENT_IN_DB_TOPIC:
-                            # if any logic should be computed on the incoming data, we should do it here?
                             event_string = mqtt_message.payload.decode("utf-8")
                             self.__PGLmodel.storeJourney(event_string)
 
-                        # store produc in database (from web request)
+                        # store product in database (from web request)
+                        # publishes to indicate if product is stored succesfully, either 'VALID' or 'INVALID'
                         case self.__REQUEST_CREATE_PRODUCT_TOPIC:
                             event_string = mqtt_message.payload.decode("utf-8")
-                            success, user = self.__PGLmodel.storeProduct(event_string)
-                            self.__mqtt_client.publish(f'{self.__RESPONSE_VALIDATE_TOPIC}/{user}/response', success)               
-                            print(f'Stores product: {success}')
+                            success, user = self.__PGLmodel.storeProduct(
+                                event_string)
+                            self.__mqtt_client.publish(
+                                f'{self.__RESPONSE_VALIDATE_TOPIC}/{user}/response', success)
+                            print(f'Stored product: {success}')
 
-                        # store user in database
+                        # store user in database (from web request)
+                        # publishes to indicate if user is stored succesfully, either 'VALID' or 'INVALID'
                         case self.__REQUEST_STORE_USER_IN_DB_TOPIC:
-                            # if any logic should be computed on the incoming data, we should do it here
                             event_string = mqtt_message.payload.decode("utf-8")
-                            succ, user = self.__PGLmodel.storeUser(event_string)
-                            # publish to indicate if user is stored succesfully
-                            self.__mqtt_client.publish(f'{self.__RESPONSE_VALIDATE_TOPIC}/{user}/response', succ)               
+                            succ, user = self.__PGLmodel.storeUser(
+                                event_string)
+                            self.__mqtt_client.publish(
+                                f'{self.__RESPONSE_VALIDATE_TOPIC}/{user}/response', succ)
                             print(f'Validated user: {succ}')
 
-                        # return all events from database for given user
+                        # return all journies from database for given user
                         case self.__REQUEST_GET_EVENTS_TOPIC:
                             # retrieve data from database using the model
                             user = mqtt_message.payload.decode("utf-8")
                             data, user = self.__PGLmodel.getJourneys(user)
                             # publish the data on the proper topic
-                            self.__mqtt_client.publish(f"{self.__RESPONSE_SEND_EVENTS_TOPIC}/{user}/response", data)
+                            self.__mqtt_client.publish(
+                                f"{self.__RESPONSE_SEND_EVENTS_TOPIC}/{user}/response", data)
                             print("Published data")
 
                         # validate a user
                         case self.__REQUEST_VALIDATE_USER_TOPIC:
                             credentials = mqtt_message.payload.decode("utf-8")
-                            validity, client_id = self.__PGLmodel.validateUser(
+                            validity, user = self.__PGLmodel.validateUser(
                                 credentials)
                             self.__mqtt_client.publish(
-                                f'{self.__RESPONSE_VALIDATE_TOPIC}/{client_id}/response', validity)                        
+                                f'{self.__RESPONSE_VALIDATE_TOPIC}/{user}/response', validity)
                             print(f'Validated user: {validity}')
 
-                        # store emergency message
+                        # store emergency message in database from pi
                         case self.__REQUEST_EMERGENCY_TOPIC:
                             event_string = mqtt_message.payload.decode("utf-8")
                             self.__PGLmodel.storeEmergency(
                                 event_string)
 
+                        # return all emergencies from database for given user
                         case self.__REQUEST_GET_EMERGENCIES_TOPIC:
                             payload = mqtt_message.payload.decode("utf-8")
-                            data, user = self.__PGLmodel.getEmergencies(payload)
-                            self.__mqtt_client.publish(f'{self.__RESPONSE_EMERGENCY_TOPIC}/{user}/response', data)
+                            data, user = self.__PGLmodel.getEmergencies(
+                                payload)
+                            self.__mqtt_client.publish(
+                                f'{self.__RESPONSE_EMERGENCY_TOPIC}/{user}/response', data)
                             print("Published emergencies")
 
                         case _:
